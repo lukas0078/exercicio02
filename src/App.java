e101a98265cceeaf3656b856fc618597fb5412f5
lukas0078 <rufinolukas0@gmail.com>
2023-04-10 00:02:24 -0300

    primeiro comit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/* Nome do Aluno: Lukas santos rufino
+* RA: 12723134472
+* Data: 07/04/2023 */
+
+public class App {
+	        public static void main(String args[]){
+       
+         Scanner entrada = new Scanner(System.in);
+       
+         System.out.print("Qual seu numero? ");
+         int numero = entrada.nextInt();
+       
+         System.out.println("O seu numero foi : [" + numero + "]");
+       
+        }
+}
